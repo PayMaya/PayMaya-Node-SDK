65211bc441c09ce7e061cb247c7b205f76c4fe3c
Harold Calayan <haroldcalayan@yahoo.com>
2016-06-29 13:53:34 +0800

    Add package.json and index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports.PaymayaSDK = require('./lib/paymaya/PaymayaSDK');
+module.exports.Checkout = require('./lib/paymaya/api/Checkout');
+module.exports.Customization = require('./lib/paymaya/api/Customization');
+module.exports.Payments = require('./lib/paymaya/api/Payments');
+module.exports.Webhook = require('./lib/paymaya/api/Webhook');
+module.exports.APIManager = require('./lib/paymaya/core/APIManager');
+module.exports.Constants = require('./lib/paymaya/core/Constants');
+module.exports.HttpConfig = require('./lib/paymaya/core/HttpConfig');
+module.exports.HttpConnection = require('./lib/paymaya/core/HttpConnection');
+module.exports.PaymayaApiError = require('./lib/paymaya/core/PaymayaApiError');
+module.exports.Address = require('./lib/paymaya/model/checkout/Address');
+module.exports.Buyer = require('./lib/paymaya/model/checkout/Buyer');
+module.exports.Contact = require('./lib/paymaya/model/checkout/Contact');
+module.exports.Item = require('./lib/paymaya/model/checkout/Item');
+module.exports.ItemAmount = require('./lib/paymaya/model/checkout/ItemAmount');
+module.exports.ItemAmountDetails = require('./lib/paymaya/model/checkout/ItemAmountDetails');
