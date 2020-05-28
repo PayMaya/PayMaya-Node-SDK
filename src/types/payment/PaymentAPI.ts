dc70c897a32ea1f4ad013590ab60617a18c579b3
Hyubs Ursua <hyubs.u@gmail.com>
2020-05-28 18:56:58 +0800

    Added get() and captureAuthorizedPayment() in PaymentAPI interface

--- src/types/payment/PaymentAPI.ts
+++ src/types/payment/PaymentAPI.ts
@@ -1,11 +1,16 @@
 import PGResponse from '../core/PGResponse';
 import GetStatusResponse from './GetStatusResponse';
+import Payment from './Payment';
 import WebhookAPI from './webhook/WebhookAPI';
 
 interface PaymentAPI {
   Webhook: WebhookAPI;
 
+  get(paymentId: string): Promise<PGResponse<Payment>>;
+
   getStatus(paymentId: string): Promise<PGResponse<GetStatusResponse>>;
+
+  captureAuthorizedPayment(paymentId: string): Promise<PGResponse<Payment>>;
 }
 
 export default PaymentAPI;
